47d764a331bcd354739d1140f55a792708ee08e0
Shahid Siddiqui <shahid189817@st.jmi.ac.in>
2024-06-30 03:54:35 +0530

    v3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function Square() {
-  const [value, setValue] = useState(null);
+    const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick() {
     setValue('X');
